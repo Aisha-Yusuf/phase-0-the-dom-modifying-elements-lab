527d4124755062f6bad0f4aa5b06547fad00f45d
Aisha-Yusuf <ayshaofficiel@gmail.com>
2023-10-08 05:05:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+
+const champ = document.createElement('p')
+newHeader.innerHTML = "Your-Name is the champion"
+
+newHeader.appendChild(champ)
